7de6aa6c5c7c8e708fc00a5f0a4e8d9b622b7ac1
Meredith Rawls <mrawls@uw.edu>
2019-02-20 15:39:31 -0800

    Test that ApPipeTask runs all the subtasks.

--- tests/test_appipe.py
+++ tests/test_appipe.py
@@ -21,8 +21,15 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+import os
 import unittest
+from unittest.mock import patch
+
 import lsst.utils.tests
+import lsst.pex.exceptions as pexExcept
+import lsst.daf.persistence as dafPersist
+
+from lsst.ap.pipe import ApPipeTask
 
 
 class PipelineTestSuite(lsst.utils.tests.TestCase):
@@ -32,11 +39,34 @@ class PipelineTestSuite(lsst.utils.tests.TestCase):
     TODO: write tests for DM-13567.
     '''
 
-#    def testDoProcessCcd(self):
-#        # test something
+    @classmethod
+    def setUpClass(cls):
+        try:
+            cls.datadir = lsst.utils.getPackageDir("ap_pipe_testdata")
+        except pexExcept.NotFoundError:
+            raise unittest.SkipTest("ap_pipe_testdata not set up")
 
-#    def testDoDiffIm(self):
-#        # test something
+    def setUp(self):
+        self.butler = dafPersist.Butler(inputs={'root': self.datadir})
+        self.config = ApPipeTask.ConfigClass()
+        self.config.load(os.path.join(self.datadir, "config", "apPipe.py"))
+        self.config.ppdb.db_url = "sqlite://"
+        self.config.ppdb.isolation_level = "READ_UNCOMMITTED"
+        self.dataId = {"visit": 413635, "ccdnum": 42}
+
+    def testGenericRun(self):
+        # test the normal workflow of each ap_pipe step
+        task = ApPipeTask(self.butler, config=self.config)
+        inputRef = self.butler.dataRef("raw", **self.dataId)
+        with patch.object(task, "ccdProcessor") as mockCcdProcessor:
+            with patch.object(task, "differencer") as mockDifferencer:
+                with patch.object(task, "associator") as mockAssociator:
+                    with patch.object(task, "diaForcedSource") as mockForcedSource:
+                        task.runDataRef(inputRef)
+                        mockCcdProcessor.runDataRef.assert_called_once()
+                        mockDifferencer.runDataRef.assert_called_once()
+                        mockAssociator.run.assert_called_once()
+                        mockForcedSource.run.assert_called_once()
 
 
 class MemoryTester(lsst.utils.tests.MemoryTestCase):
